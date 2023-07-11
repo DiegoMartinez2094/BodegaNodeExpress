801a032a6e477acaddf83cf15910f1eb46ede5c9
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-07-11 15:10:56 -0500

    feat: :rocket: creación del endpoint productos
    Con el endpoint producto podemos insertar, visualizar, eliminar y editar  productos de la tabla

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import usuarios from './routers/users.js'
 import bodegas from './routers/bodegas.js'
 import inventarios from './routers/inventarios.js'
+import productos from './routers/productos.js'
 
 import express from 'express';
 import dotenv from "dotenv";
@@ -17,3 +18,4 @@ appExpress.listen(config, ()=>{
 appExpress.use("/usuarios",usuarios)
 appExpress.use("/bodegas",bodegas)
 appExpress.use("/inventarios",inventarios)
+appExpress.use("/productos",productos)
---
+++ routers/productos.js
@@ -0,0 +1,80 @@
+import { Router } from "express";
+import mysql from "mysql2";
+const appProductos = Router();
+let con = undefined;
+
+// conexion a base de datos
+appProductos.use((req, res, next)=>{
+    try {
+        con = mysql.createPool({
+            host: "localhost",
+            user: "root",
+            password: "123456",
+            database: "db_prueba_backend_sql",
+            port: 3306
+    });
+    next();        
+    } catch (error) {
+        res.status(500).send('Conexion pailas papá, funado de la red :(')
+    }
+})
+
+appProductos.post("/", (req, res) => {
+    con.query(
+        /*sql*/ `INSERT INTO productos SET ?`,
+        req.body,
+        (err,data,fils)=>{
+            console.log(err);
+            console.log(data);
+            console.log(fils);
+            data.affectedRows+=200;
+            let result = req.body;
+            result.id= data.insertId;
+            res.status(data.affectedRows).send(result);
+        }
+    );
+
+})
+
+appProductos.get("/", (req, res) => {
+    con.query(
+        /*sql*/ `SELECT * FROM productos`,
+        req.body,
+        (err,data,fils)=>{
+            console.log(err);
+            console.log(data);
+            console.log(fils);
+            res.send(data)
+        }
+    );
+})
+
+appProductos.put("/:id", (req, res) => {
+    con.query(
+        /*sql*/ `UPDATE productos SET ? WHERE id= ?`,
+        [req.body, req.params.id],
+        (err,data,fils)=>{
+            console.log(err);
+            console.log(data);
+            console.log(fils);
+            res.send(data)
+        }
+    );
+})
+
+appProductos.delete("/:id", (req, res) => {
+    con.query(
+        /*sql*/ `DELETE FROM productos WHERE id= ?`,
+        req.params.id,
+        (err,data,fils)=>{
+            console.log(err);
+            console.log(data);
+            console.log(fils);
+            res.send(data)
+        }
+    );
+})
+
+
+
+export default appProductos;
